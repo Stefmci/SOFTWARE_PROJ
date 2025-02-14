b7b8537ef392398bcd4a7d5a5d0f79c127cb017c
akoca080 <ka5936@mci4me.at>
2025-02-14 15:27:56 +0100

    Die letzten beiden Klassen wurden implementiert. Grundgerüst für die Simulation wurde hinzugefügt.

--- stangen.py
+++ stangen.py
@@ -3,10 +3,12 @@ import numpy as np
 class Stangen:
     def __init__(self, p1, p2):
         """Erstellt eine Stange zwischen zwei Punkten."""
-        self.p1 = p1
-        self.p2 = p2
-        self.länge = np.linalg.norm(self.punkt2.get_position() - self.punkt1.get_position())
+        self.p1 = p1  # Startpunkt (Objekt vom Typ Punkt)
+        self.p2 = p2  # Endpunkt (Objekt vom Typ Punkt)
+
+        # Berechnung der Länge anhand der Koordinaten
+        self.länge = np.linalg.norm(np.array(self.p2.get_position()) - np.array(self.p1.get_position()))
 
     def get_endpunkte(self):
         """Gibt die Koordinaten der Endpunkte der Stange zurück."""
-        return self.punkt1.get_position(), self.punkt2.get_position()
+        return self.p1.get_position(), self.p2.get_position()
